4e3780a4c441e39e92bf36a43cc17a8a45d4eb9d
siva209 <sivanarayana209@gmail.com>
2021-07-01 06:15:51 +0530

    Added:Functionality to calculate total wage using reduce method

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -36,13 +36,26 @@ while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
     totalWorkingDays < NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
-    empDailyWageArr.push(calcDailyWage(totalEmpHrs));
+    let workingHrs = getWorkingHours(empCheck);
+    totalEmpHrs += workingHrs;
+    empDailyWageArr.push(calcDailyWage(workingHrs));
 }
 let totalEmpWage = totalEmpHrs * WAGE_PER_HOUR;
 console.log("UC5-Total Days : " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Wage : " + totalEmpWage);
 console.log(empDailyWageArr);
 //UC6
-function calcDailyWage(empHrs){
+function calcDailyWage(empHrs) {
     return empHrs * WAGE_PER_HOUR;
 }
+//UC7A - Calc total Wage using Array forEach traversal or reduce method
+let totEmpWage = 0;
+function sum(dailyWage) {
+    totEmpWage += dailyWage;
+}
+empDailyWageArr.forEach(sum);
+console.log('UC7A - Total Days : ' + totalWorkingDays + 'Total Hrs : ' + totalEmpHrs + 'Emp Wage : ' + totalEmpWage);
+
+function totalWages(totalWage, dailyWage) {
+    return totalWage + dailyWage;
+}
+console.log('UC7A - Emp Wage with reduce : ' + empDailyWageArr.reduce(totalWages, 0));
