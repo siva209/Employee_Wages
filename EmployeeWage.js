0a50871d56a9afc76882616389e8f3d47cbf0f84
siva209 <sivanarayana209@gmail.com>
2021-07-03 07:36:38 +0530

    Added:Functionality to display the full time part time and non working days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -110,4 +110,21 @@ let count = 0;
 let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal, 0);
 let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0).reduce(findTotal, 0);
 console.log('UC 9A - Emp Wage with Arrow : ' + 'Total Hours : ' + totalHours + ' Total Wage : ' + totalSalary);
-
+//UC 9B - Display full Working days, part working days and non working days
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+empDailyHrsMap.forEach((value,key)=>{
+    if(value==8){
+        fullWorkingDays.push(key);
+    }
+    else if(value == 4){
+        partWorkingDays.push(key);
+    }
+    else{
+        nonWorkingDays.push(key);
+    }
+});
+console.log('Full Working Days : ' + fullWorkingDays);
+console.log('Part Working Days : ' + partWorkingDays);
+console.log('Non Working Days ' + nonWorkingDays);
