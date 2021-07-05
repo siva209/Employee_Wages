20c41ef198df606aacc3e512d05b5a0fe8a29fea
siva209 <sivanarayana209@gmail.com>
2021-07-05 18:53:59 +0530

    Added:Functionality to validate starting part of email

--- patterns.js
+++ patterns.js
@@ -1,7 +1,7 @@
 function validate(zip) {
-    let zipRegex = RegExp('^[0-9]{6}$');
+    let zipRegex = RegExp('^[0-9]{3}[ ]?[0-9]{3}$');
     if (zipRegex.test(zip)) {
-        console.log("Zip is correct");
+        console.log("Zip is correct : ");
     } else {
         console.log("Zip is incorrect");
     }
@@ -9,3 +9,14 @@ function validate(zip) {
 var prompt = require('prompt-sync')();
 let zip = prompt("Enter the zip : ");
 validate(zip);
+//Validating Email
+function validateEmail(email) {
+    let emailRegex = RegExp('^[a-zA-Z0-9_]+');
+    if (emailRegex.test(email)) {
+        console.log("Email is correct");
+    } else {
+        console.log("Email is incorrect");
+    }
+}
+let email = prompt("Enter the email : ");
+validateEmail(email);
